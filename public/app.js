d70eaeb43e527d578e67c212f78d917053c8d6eb
Nikita Abakumov <nabakumov@mt.1tv.ru>
2022-07-15 23:34:57 +0300

    edit improve

--- public/app.js
+++ public/app.js
@@ -1,17 +1,46 @@
 document.addEventListener('click',  (event) => {
-    if (event.target.dataset.type === 'remove') {
+    const type = event.target.dataset.type
+
+    const saveBtn = event.target.closest('div').querySelector('[data-type=save]');
+    const cancelBtn = event.target.closest('div').querySelector('[data-type=cancel]');
+    const editBtn = event.target.closest('div').querySelector('[data-type=edit]');
+    const removeBtn = event.target.closest('div').querySelector('[data-type=remove]');
+    const title = event.target.closest('li').querySelector('[data-type=title]');
+    const titleInput = event.target.closest('li').querySelector('[data-type=title-input]');
+
+    if (type === 'remove') {
         const id = event.target.dataset.id
         remove(id).then(() => {
             event.target.closest('li').remove()
         })
-    } else if (event.target.dataset.type === 'edit') {
+    }
+    else if (type === 'edit') {
+        toogleEdit()
+        const titleValue = title.textContent
+        titleInput.value = titleValue.trim()
+        titleInput.focus()
+    } else if (type === 'save') {
+        toogleEdit()
         const id = event.target.dataset.id
-        const newTitle = prompt('Input new title')
+        const newTitle = titleInput.value.trim()
         if (newTitle) {
             edit(id, newTitle).then(() => {
-                event.target.closest('li').firstChild.textContent = newTitle
+                event.target.closest('li')
+                    .querySelector('[data-type=title]').textContent = newTitle
             })
         }
+
+    } else if (type === 'cancel') {
+        toogleEdit()
+    }
+
+    function toogleEdit() {
+        saveBtn.classList.toggle('d-none')
+        cancelBtn.classList.toggle('d-none')
+        editBtn.classList.toggle('d-none')
+        removeBtn.classList.toggle('d-none')
+        title.classList.toggle('d-none')
+        titleInput.classList.toggle('d-none')
     }
 })
 
